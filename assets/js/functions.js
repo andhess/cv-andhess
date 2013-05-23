67aa75a881205b15e815f1b6e5dabda91235b98b
andhess <andrewclaytonhess@gmail.com>
2013-05-23 03:26:22 -0400

    complete rewrite of entire site in js, allows it to run on a static site. probably didn't need js functions, but just carried over what philip had done and went with it. looks the exact same, except I changed the font on mine.

---
+++ assets/js/functions.js
@@ -0,0 +1,104 @@
+function position( company , city , job, dates, blob, link, list ){
+    var output = '';
+
+    output += '<div class="row" style="padding: 35px 0 4px 0;">';
+    output += '<div class="span8"><h4 style="margin: 0">' + company;
+            
+    if( link.length > 0){
+        output += '<a href="' + link + '" target="_BLANK">&nbsp;&nbsp;&nbsp;<span class="badge">  Website <i class="icon-arrow-right icon-white"></i></span></a>';   
+    }
+    output += '</h4>';
+    output += '</div>';
+    output += '<div class="span4">';
+    output += '<h4 class="pull-right hidden-phone" style="margin: 0">' + city + '</h4>';
+    output += '</div>';
+    output += '</div>';  // <!-- end row-->
+    output += '<div class="row">';
+    output += '<div class="span6">';
+    output += '<span style="font-style:italic; font-size: 15px;">' + job + '</span>';
+    output += '</div>';
+    output += '<div class="span6">';
+    output += '<span class="visible-phone label">' + dates + '</span>';
+    output += '<span class="hidden-phone pull-right" style="font-style:italic; font-size: 15px;" >' + dates + '</span>';        
+    output += '</div>';
+    output += '</div>';  // ends row
+
+    if( blob.length > 0 ){
+        output += '<div class="row" style="padding-top: 8px;">';
+        output += '<div class="span12">';
+        output += '<ul>';
+
+        if( list == 1 ){
+            for( var i = 0 ; i <= blob.length - 1 ; i++ ){
+                output += '<li><span style="font-weight: bold">' + blob[i][0] + ': </span>';
+                var liSize = blob[i][1].length - 1;
+
+                for( var j = 0 ; j <= liSize ; j++ ){
+                    if( j < liSize ){
+                        output += blob[i][1][j] + ', ';
+                    }
+                    else{
+                        output += blob[i][1][liSize];
+                    }
+                };
+                output += '</li>';
+            };
+        }
+        else{
+            for( var i = 0 ; i <= blob.length - 1 ; i++ ){
+                output += '<li><span style="font-weight: bold">' + blob[i] + '</span></li>';
+            };
+        }
+    output += '</ul></div></div>'; // end row
+    }
+
+    document.write( output );
+    document.close();
+}
+
+function skillShow( skill, percent ){
+    var output = '';
+
+    output += '<div class="row"><div class="span3"';
+    output += '<p><h4 style="text-align: right" class="hidden-phone">' + skill + '</h4></p>';
+    output += '<p><span class="badge visible-phone" style="text-align: center">' + skill + '</span></p></div>';
+    output += '<div class="span4"><div class="progress" style="margin: 10px 0 10px 0; height: 22px;">';
+    output += '<div class="bar" style="width:' + percent +'%;"></div>';
+    output += '</div></div><div class="span1"></div></div>';
+
+    document.write( output );
+    document.close();
+}
+
+function extra( title, blob ){
+    var output = '';
+
+    output += '<div class="row" style="padding-top:30px"><div class="span3">';
+    output += '<span class="pull-right hidden-phone"><strong>' + title + '</strong></span>';
+    output += '<span class="visible-phone badge">' + title + '</span></div>';
+    output += '<div class="span5">' + blob + '</div></div>';
+
+    document.write( output );
+    document.close();
+}
+
+function project( name, liveLink, code ){
+    var output = '';
+
+    output += '<li style="padding-top: 12px">';
+    
+    if( liveLink.length > 0 ){
+        output += '<a href="' + liveLink + '" target="_BLANK" >' + name + '</a>';
+    }
+    else {
+        output += name;
+    }
+    
+    if( code.length > 0 ){
+        output += '<a href="' + code + '" target="_BLANK"">&nbsp;&nbsp;&nbsp;<span class="badge badge-info">Code <i class="icon-arrow-right icon-white"></i></span></a>';
+    }
+    output += '</li>';
+
+    document.write( output );
+    document.close();
+}
--- index.php
+++ index.php
@@ -24,11 +24,11 @@
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="assets/ico/favicon.ico">
-	<script>
+    <script>
 
-		/** ADD MY GOOGLE ANALYTICS SHIT HERE */
+        /** ADD MY GOOGLE ANALYTICS SHIT HERE */
 
-	</script>
+    </script>
   
   </head>
 
@@ -72,25 +72,25 @@
     </div>
 -->
     <div class="container">
-	
-	<header class="jumbotron">
-		
+    
+    <header class="jumbotron">
+        
     <div class="row">
-	<div class="span12">
-		<h3 style="text-align: center; text-style: italic">Curriculum Vitae for</h3>
+    <div class="span12">
+        <h3 style="text-align: center; text-style: italic">Curriculum Vitae for</h3>
 
-<h1 style="padding: 10px 0px; text-align: center; font-size: 60px; line-height: 70px;">Andrew Clayton Hess</h1>	
-	<span class="" style="text-align: center;"> 
-		<address>
-		<a href="http://www.andrewhess.net" target="_blank">www.andrewhess.net</a><br />
-		<a href="mailto:andrew@andrewhess.net">andrew@andrewhess.net</a><br />
-		<a>+1 (248) 224-0335</a>
-		</address>
-	</span>
-		</div>
+<h1 style="padding: 10px 0px; text-align: center; font-size: 60px; line-height: 70px;">Andrew Clayton Hess</h1> 
+    <span class="" style="text-align: center;"> 
+        <address>
+        <a href="http://www.andrewhess.net" target="_blank">www.andrewhess.net</a><br />
+        <a href="mailto:andrew@andrewhess.net">andrew@andrewhess.net</a><br />
+        <a>+1 (248) 224-0335</a>
+        </address>
+    </span>
+        </div>
     </div> 
 
-	</header>
+    </header>
 
 <section id="about">
       <div class="row">
@@ -136,48 +136,48 @@ position("Washington University in Saint Louis","Saint Louis, MO","Bachelor of S
     <?php
 
 
-	position("Expensify Inc.","San Francisco, CA","Engineering Intern","5/12-8/12",'
-	<ul>
-	<li>Worked on various aspects of the Expensify product including website and mobile applications.</li>
-	<li>Developed in multiple languages including php, JavaScript, Objective-C, and Java.</li>
-	</ul>','https://www.expensify.com/');
-	
-	position("Washington University Department of Electrical and Systems Engineering","Saint Louis, MO","Undergraduate Research","1/13-present",'
-	<ul>
-	<li>Currently working on developing charging algorithms for electrical vehicles.</li>
-	<li>Research consists of devising algorithms, creating mathematical models, and runnning simulations.</li>
-	<li>All models and simulations built in Python.</li>
-	</ul>',"");
+    position("Expensify Inc.","San Francisco, CA","Engineering Intern","5/12-8/12",'
+    <ul>
+    <li>Worked on various aspects of the Expensify product including website and mobile applications.</li>
+    <li>Developed in multiple languages including php, JavaScript, Objective-C, and Java.</li>
+    </ul>','https://www.expensify.com/');
+    
+    position("Washington University Department of Electrical and Systems Engineering","Saint Louis, MO","Undergraduate Research","1/13-present",'
+    <ul>
+    <li>Currently working on developing charging algorithms for electrical vehicles.</li>
+    <li>Research consists of devising algorithms, creating mathematical models, and runnning simulations.</li>
+    <li>All models and simulations built in Python.</li>
+    </ul>',"");
 
-	position("TextReject.com","Saint Louis, MO","Co-Founder","10/12-12/12",'
-	<ul>
-	<li>Designed, built, and launched a humorous php application that connects with Twilio for an automated text messaging service.</li>
-	<li>A semi-finalist in the 2013 South by Southwest Student Startup Madness competition.</li>
-	</ul>',"http://textreject.com/");
-	
-	
-	position("CapScan","Saint Louis, MO","Co-Founder","12/11-4/12",'
-	<ul>
-	<li>Developed an Android application to collect coupon codes from pictures using <abbr title="Optical Character Recognition">OCR</abbr> technology.</li>
-	<li>Developed image processing algorithms to make it possible for <a href="http://code.google.com/p/tesseract-ocr/" target="_BLANK">Tesseract</a> to process the text in images.</li>
-	<li>Project was discontinued and never released due to the difficulty involved with developing accurate and reliable OCR.</li>
-	</ul>',"");
-	
-	
-	position("Washington University Tech Entrepreneurs (wute)","Saint Louis, MO","Co-President","5/11-Present",'
-	<ul>
-	<li>wute is a community of students interested in technology and entrepreneurship.</li>
-	<li>Help coordinate meetups that feature a small, informal discussion with people from the tech industry.</li>
-	<li>Past speakers include <a href="http://en.wikipedia.org/wiki/Alexis_Ohanian" target="_BLANK">Alexis Ohanian</a>, <a href="https://twitter.com/tristan" target="_BLANK">Tristan O\'Tierney</a>, <a href="http://www.samwen.com/" target="_BLANK">Sam Wen</a></li>
-	<li>Currently working with Washington University\'s <a href="http://engineering.wustl.edu/disc-comp.aspx" target="_BLANK">Discovery Competition</a> to create the first ever student-run incubator.</li>
-	</ul>', "http://wute.org");
+    position("TextReject.com","Saint Louis, MO","Co-Founder","10/12-12/12",'
+    <ul>
+    <li>Designed, built, and launched a humorous php application that connects with Twilio for an automated text messaging service.</li>
+    <li>A semi-finalist in the 2013 South by Southwest Student Startup Madness competition.</li>
+    </ul>',"http://textreject.com/");
+    
+    
+    position("CapScan","Saint Louis, MO","Co-Founder","12/11-4/12",'
+    <ul>
+    <li>Developed an Android application to collect coupon codes from pictures using <abbr title="Optical Character Recognition">OCR</abbr> technology.</li>
+    <li>Developed image processing algorithms to make it possible for <a href="http://code.google.com/p/tesseract-ocr/" target="_BLANK">Tesseract</a> to process the text in images.</li>
+    <li>Project was discontinued and never released due to the difficulty involved with developing accurate and reliable OCR.</li>
+    </ul>',"");
+    
+    
+    position("Washington University Tech Entrepreneurs (wute)","Saint Louis, MO","Co-President","5/11-Present",'
+    <ul>
+    <li>wute is a community of students interested in technology and entrepreneurship.</li>
+    <li>Help coordinate meetups that feature a small, informal discussion with people from the tech industry.</li>
+    <li>Past speakers include <a href="http://en.wikipedia.org/wiki/Alexis_Ohanian" target="_BLANK">Alexis Ohanian</a>, <a href="https://twitter.com/tristan" target="_BLANK">Tristan O\'Tierney</a>, <a href="http://www.samwen.com/" target="_BLANK">Sam Wen</a></li>
+    <li>Currently working with Washington University\'s <a href="http://engineering.wustl.edu/disc-comp.aspx" target="_BLANK">Discovery Competition</a> to create the first ever student-run incubator.</li>
+    </ul>', "http://wute.org");
 
 
-	position("Pontiac Yacht Club","Keego Harbor, MI","Junior Sailing Head Coach","6/08-08/11",'
-	<ul>
-	<li>Managed and coached a team of 25 junior sailors, ages 8 to 18.</li>
-	<li>Travelled weekly to local and regional events over a 12 week season.</li>
-	</ul>', "http://pyc54.org/");
+    position("Pontiac Yacht Club","Keego Harbor, MI","Junior Sailing Head Coach","6/08-08/11",'
+    <ul>
+    <li>Managed and coached a team of 25 junior sailors, ages 8 to 18.</li>
+    <li>Travelled weekly to local and regional events over a 12 week season.</li>
+    </ul>', "http://pyc54.org/");
 
 
            ?>
@@ -195,9 +195,9 @@ position("Washington University in Saint Louis","Saint Louis, MO","Bachelor of S
       </div>
     </div> <!-- end row -->
 <div class="row">
-	<h3>Systems and Electrical Engineering</h3>
-	
-	<div class="span8">
+    <h3>Systems and Electrical Engineering</h3>
+    
+    <div class="span8">
 <?php
 iprogram('Control Engineering','75');
 iprogram('Signal Processing', '65');
@@ -213,13 +213,13 @@ iprogram('Mathematical Modeling', '75');
       
       
             <div class="row">
-            	<h3>Computer Science</h3>
+                <h3>Computer Science</h3>
       
     </div> <!-- end row -->
 <div class="row">
 
-	
-	<div class="span8">
+    
+    <div class="span8">
 <?php
 iprogram('JavaScript','75');
 iprogram('PHP','70');
@@ -238,16 +238,16 @@ iprogram('Adobe CS6','50');
 
 </div>
 <div class="span4">
-	<div class="well">
-		<h4>Selected Projects</h4>
-		<hr />
-		<ul>
-		<?php
-			project('TextReject.com','http://www.textreject.com', 'https://github.com/textreject' );
-		?>
-		</ul>
-		<a class="pull-right btn btn-small btn-inverse" href="http://github.com/andhess">Github Profile <i class="icon-arrow-right icon-white"></i></a><br />
-	</div>
+    <div class="well">
+        <h4>Selected Projects</h4>
+        <hr />
+        <ul>
+        <?php
+            project('TextReject.com','http://www.textreject.com', 'https://github.com/textreject' );
+        ?>
+        </ul>
+        <a class="pull-right btn btn-small btn-inverse" href="http://github.com/andhess">Github Profile <i class="icon-arrow-right icon-white"></i></a><br />
+    </div>
 </div>
 </div>
       
@@ -261,38 +261,38 @@ iprogram('Adobe CS6','50');
       <h2>Honors and Skills</h2>
       </div>
     </div> <!-- end row -->
-	<div class="row">
-	<div class="span8">
-	<?php
-	
-	extra('Awards','2010 Olin Business School - Lionsgate Films Case Competition Winner');
-	extra('Language','Familiar with Spanish, some exposure to French');
-	
-	?>
-	</div>
-	
-	<div class="span4">
-	<div class="well">
-		<h4>Selected Press</h4>
-		<hr />
-		<ul>
-			<li>Techli: <a href="http://techli.com/text-reject-SXSW">St. Louis-based Text Reject Chosen For SXSW Student Startup Madness Competition </a></li>
-			<li>St. Louis Business Journal: <a href="http://www.bizjournals.com/stlouis/blog/BizNext/2012/12/3-st-louis-student-startups-advance.html?ana=twt">3 St. Louis student startups advance in national competition </a></li>
-			<li>St. Louis Business Journal: <a href="http://www.bizjournals.com/stlouis/blog/BizNext/2012/11/wash-u-students-launch-text-reject.html">WashU Students Launch Text Reject </a></li>
-			<li>Student Life: <a href= "http://www.studlife.com/news/2012/11/15/you-are-unwanted-student-business-offers-humorous-text-rejections/">You are unwanted: Student business offers humorous text rejections </a></li>
-			<li>Fox 2 Now: <a href="http://fox2now.com/2012/10/31/text-reject/">Interview on Fox 2 News St. Louis for Text Reject </a></li>
-		</ul>
-		<!--<a class="pull-right btn btn-small btn-inverse" href="http://philipithomas.com/press">View Full Press <i class="icon-arrow-right icon-white"></I></a><br /> -->
-	</div>
-	</div>
+    <div class="row">
+    <div class="span8">
+    <?php
+    
+    extra('Awards','2010 Olin Business School - Lionsgate Films Case Competition Winner');
+    extra('Language','Familiar with Spanish, some exposure to French');
+    
+    ?>
+    </div>
+    
+    <div class="span4">
+    <div class="well">
+        <h4>Selected Press</h4>
+        <hr />
+        <ul>
+            <li>Techli: <a href="http://techli.com/text-reject-SXSW">St. Louis-based Text Reject Chosen For SXSW Student Startup Madness Competition </a></li>
+            <li>St. Louis Business Journal: <a href="http://www.bizjournals.com/stlouis/blog/BizNext/2012/12/3-st-louis-student-startups-advance.html?ana=twt">3 St. Louis student startups advance in national competition </a></li>
+            <li>St. Louis Business Journal: <a href="http://www.bizjournals.com/stlouis/blog/BizNext/2012/11/wash-u-students-launch-text-reject.html">WashU Students Launch Text Reject </a></li>
+            <li>Student Life: <a href= "http://www.studlife.com/news/2012/11/15/you-are-unwanted-student-business-offers-humorous-text-rejections/">You are unwanted: Student business offers humorous text rejections </a></li>
+            <li>Fox 2 Now: <a href="http://fox2now.com/2012/10/31/text-reject/">Interview on Fox 2 News St. Louis for Text Reject </a></li>
+        </ul>
+        <!--<a class="pull-right btn btn-small btn-inverse" href="http://philipithomas.com/press">View Full Press <i class="icon-arrow-right icon-white"></I></a><br /> -->
+    </div>
+    </div>
 
-	</div>
+    </div>
 
 
       </section>
 
       <footer style="padding-top: 20px;">
-	<hr />
+    <hr />
       <p style="text-align: center">&copy; Andrew Hess 2013</p>
       </footer>      
 
@@ -316,89 +316,89 @@ These are some functions to make uniform sections
 */
 
 function position($company,$city,$job,$dates,$blob,$link) {
-	/*
-	This function is for use in the experience section. It unififes formatting for the various positions. 
-	*/
-	
-	echo '
-		<div class="row" style="padding: 35px 0 4px 0;">
-			<div class="span8"><h4 style="margin: 0">'.$company;
-				if (!empty($link)) {
-					echo ' <a href="'.$link.'" target="_BLANK"><span class="badge">Website <i class="icon-arrow-right icon-white"></i></span></a>';
-				}
-			echo '</h4> 
-			
-			</div>	
-			<div class="span4"><h4 class="pull-right hidden-phone" style="margin: 0">'.$city.'</h4>
-			</div>
-			
-		</div> <!-- end row-->
-			
-		<div class="row">
-			<div class="span6"><span style="font-style:italic; font-size: 15px;">'.$job.'</span></div>	
-			<div class="span6">
-			<span class="visible-phone label">'.$dates.'</span>
-			<span class="hidden-phone pull-right" style="font-style:italic; font-size: 15px;" >'.$dates.'</span>
-			
-			</div>
-		</div> <!-- end row-->
-		';
-		if (!empty($blob)) {
-			echo '
-			<div class="row" style="padding-top: 8px;"><div class="span12">'.$blob.'</div></div>';
-			}	
-			
+    /*
+    This function is for use in the experience section. It unififes formatting for the various positions. 
+    */
+    
+    echo '
+        <div class="row" style="padding: 35px 0 4px 0;">
+            <div class="span8"><h4 style="margin: 0">'.$company;
+                if (!empty($link)) {
+                    echo ' <a href="'.$link.'" target="_BLANK"><span class="badge">Website <i class="icon-arrow-right icon-white"></i></span></a>';
+                }
+            echo '</h4> 
+            
+            </div>  
+            <div class="span4"><h4 class="pull-right hidden-phone" style="margin: 0">'.$city.'</h4>
+            </div>
+            
+        </div> <!-- end row-->
+            
+        <div class="row">
+            <div class="span6"><span style="font-style:italic; font-size: 15px;">'.$job.'</span></div>  
+            <div class="span6">
+            <span class="visible-phone label">'.$dates.'</span>
+            <span class="hidden-phone pull-right" style="font-style:italic; font-size: 15px;" >'.$dates.'</span>
+            
+            </div>
+        </div> <!-- end row-->
+        ';
+        if (!empty($blob)) {
+            echo '
+            <div class="row" style="padding-top: 8px;"><div class="span12">'.$blob.'</div></div>';
+            }   
+            
 }
 
 
 function iprogram($language,$percent) {
-	// Progress bars for technical section
-	echo '
-	<div class="row">
-		<div class="span3"
-		<p><h4 style="text-align: right" class="hidden-phone">'.$language.'</h4></p>
-		<p><span class="badge visible-phone" style="text-align: center">'.$language.'</span></p>
-		</div>
-		<div class="span4">
-	      <div class="progress" style="margin: 10px 0 10px 0; height: 22px;">
-	        <div class="bar" style="width: '.$percent.'%;"></div>
-	      </div>
-	    </div>
-		<div class="span1"></div>
-	</div>
-	';
-	
+    // Progress bars for technical section
+    echo '
+    <div class="row">
+        <div class="span3"
+        <p><h4 style="text-align: right" class="hidden-phone">'.$language.'</h4></p>
+        <p><span class="badge visible-phone" style="text-align: center">'.$language.'</span></p>
+        </div>
+        <div class="span4">
+          <div class="progress" style="margin: 10px 0 10px 0; height: 22px;">
+            <div class="bar" style="width: '.$percent.'%;"></div>
+          </div>
+        </div>
+        <div class="span1"></div>
+    </div>
+    ';
+    
 }
 
 function project($name,$look,$code) {
-	// Makes lists for displaying projects
-	echo '<li style="padding-top: 12px">';
-	
-	if (!empty($look)) {
-		// link to look at completed project
-		echo '<a href="'.$look.'" target="_BLANK" >'.$name.'</a>';
-	}
-	else {
-		echo $name;
-	}
-	
-	if (!empty($code)) {
-		// link to look at completed project
-		echo ' <a href="'.$code.'" target="_BLANK""><span class="badge badge-info">Code <i class="icon-arrow-right icon-white"></i></span></a>';
-	}
-	echo '</li>';
+    // Makes lists for displaying projects
+    echo '<li style="padding-top: 12px">';
+    
+    if (!empty($look)) {
+        // link to look at completed project
+        echo '<a href="'.$look.'" target="_BLANK" >'.$name.'</a>';
+    }
+    else {
+        echo $name;
+    }
+    
+    if (!empty($code)) {
+        // link to look at completed project
+        echo ' <a href="'.$code.'" target="_BLANK""><span class="badge badge-info">Code <i class="icon-arrow-right icon-white"></i></span></a>';
+    }
+    echo '</li>';
 }
 
 function extra($title,$blob) {
-	echo '<div class="row" style="padding-top:30px">
-				<div class="span3">
-					<span class="pull-right hidden-phone"><strong>'.$title.'</strong></span>
-					<span class="visible-phone badge">'.$title.'</span>
-				</div>
-				<div class="span5">
-					'.$blob.'
-				</div>
-			</div>';
-				
+    echo '<div class="row" style="padding-top:30px">
+                <div class="span3">
+                    <span class="pull-right hidden-phone"><strong>'.$title.'</strong></span>
+                    <span class="visible-phone badge">'.$title.'</span>
+                </div>
+                <div class="span5">
+                    '.$blob.'
+                </div>
+            </div>';
+                
 }
 ?>
